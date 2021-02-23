1ce283bc935e6804e7597b627714d8689c008ab8
conghaonan <626620755@qq.com>
2021-02-23 22:36:12 +0800

    cong

---
+++ src/main/java/com/markerhub/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.markerhub.mapper;
+
+import com.markerhub.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-02-22
+ */
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
