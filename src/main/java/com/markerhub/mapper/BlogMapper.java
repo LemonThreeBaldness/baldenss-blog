aee2c55ae4bfc04f24140837216146f07355e3ae
sihgyu <sihgyu@github123>
2021-03-05 00:08:25 +0800

    fix

---
+++ src/main/java/com/markerhub/mapper/BlogMapper.java
@@ -0,0 +1,16 @@
+package com.markerhub.mapper;
+
+import com.markerhub.entity.Blog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yusx
+ * @since 2021-03-04
+ */
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
